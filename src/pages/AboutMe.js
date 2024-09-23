f2e1367d21ad0c7ff0a6829e2cb7e1ba52509cab
Danille Hamilton <danilleham@hotmail.co.uk>
2024-09-23 20:52:48 +0000

    Add resume button

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Row } from "react-bootstrap";
 
 const AboutMe = () => {
   return (
@@ -6,14 +7,14 @@ const AboutMe = () => {
         <h1>
         Danille Hamilton
         </h1>
-        <div>
+        <Row>
             <p>
                 A picture of me
             </p>
-        </div>
-        <div>
+        </Row>
+        <Row>
     <p>
-    Hi, I’m [Your Name], a software developer with a unique background in psychology and a passion for building intuitive, user-centered digital solutions. After earning my degree in Psychology, I developed a deep interest in how technology can enhance human experiences. This curiosity led me to pursue a diploma in Software Development, where I gained hands-on experience in modern programming languages and development frameworks.
+    Hi, I’m Danille, a software developer with a unique background in psychology and a passion for building intuitive, user-centered digital solutions. After earning my degree in Psychology, I developed a deep interest in how technology can enhance human experiences. This curiosity led me to pursue a diploma in Software Development, where I gained hands-on experience in modern programming languages and development frameworks.
     <br/>
     <br/>
     As I transition into a full-time career in software development, I’m focused on creating applications that are both functional and empathetic to users' needs. My psychological insights give me a unique perspective on user experience and behavior, allowing me to design software that is not only efficient but also mindful of how people interact with technology.
@@ -21,7 +22,8 @@ const AboutMe = () => {
     <br/>
     I’m excited to combine my skills in problem-solving, critical thinking, and software development to tackle challenges in tech, and I’m always eager to learn and grow in this ever-evolving field.
     </p>
-        </div>
+    <button><p>Download resume</p></button>
+        </Row>
     </div>
   )
 }
