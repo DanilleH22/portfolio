317ee265a9cf137afb06d6db7801c524a6bedb90
Danille Hamilton <danilleham@hotmail.co.uk>
2024-09-23 21:09:58 +0000

    (project.js): reafctor coode for image and card text layout

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,50 +1,55 @@
-import React from 'react'
-import Accordion from 'react-bootstrap/Accordion';
+import React from 'react';
+import proj1 from '../assets/proj1.png';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import styles from "../styles/Projects.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 
-export default function MyAccordion() {
+const Projects = () => {
+  return (
+    <Container fluid>
+      <section id="projects">
+        {/* Project Layout with Image and Card Side by Side */}
+        <Row className="align-items-center">
+          <Col width={500} height={400}>
+            <Image src={proj1}  rounded alt="Project 1 EcoLifeHub" />
+          </Col>
+          <Col >
+            <Card style={{ width: '30rem', height: '30rem' }}>
+              <Card.Body className='project-body'>
+                <Card.Title className='project-title'>EcoLifeHub</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted project-subtitle">Front-end Developer</Card.Subtitle>
+                <Card.Text className='project-text'>
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+                </Card.Text>
+                <Card.Text className={styles.projectTechnology}>HTML</Card.Text>
+                <Card.Text className={styles.projectTechnology}>CSS</Card.Text>
+                
 
-    let data = [
-        {
-            name: "EcoLifeHub",
-            description: "Lorem ipsum"
-        },
-        {
-            name: "Simpsons Smash",
-            description: "Lorem ipsum"
-        },
-        {
-            name: "Which Tv Character Are You?",
-            description: "Lorem ipsum"
-        },
-        {
-            name: "Inkpad",
-            description: "Lorem ipsum"
-        },
-        {
-            name: "Innovevent",
-            description: "Lorem ipsum"
-        },
-    ]
+                {/* GitHub Link */}
+                <Card.Link className={styles.projectLink} href="https://github.com/DanilleH22/EcoLifeHub" target="_blank" aria-label="GitHub Link">
+                  <FontAwesomeIcon icon={faGithub} style={{ color: 'blue' }} />
+                </Card.Link>
 
-    return (
-        <div>
-          <h1> Projects </h1>
-        <Accordion defaultActiveKey="0">
-            {
-                data.map((item, index) => (
-                    <Accordion.Item eventKey={index.toString()} key={index}>
-                        <Accordion.Header>{item.name}</Accordion.Header>
-                        <Accordion.Body>
-                            {item.description}
-                        </Accordion.Body>
-                    </Accordion.Item>
-                ))
-            }
-        </Accordion>
-        </div>
-    );
-    
-} 
+                {/* Live Project Link */}
+                <Card.Link className={styles.projectLink} href="https://danilleh22.github.io/EcoLifeHub/" target="_blank" aria-label="Live Link">
+                <FontAwesomeIcon icon={faArrowUpRightFromSquare} style={{ color: 'blue' }} />
+                </Card.Link>
+                
+                
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </section>
+    </Container>
+  );
+};
 
-  
+export default Projects;
 
