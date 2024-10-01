546e1b101b51e680cb035ffdbe3cc6644fde4608
Danille Hamilton <danilleham@hotmail.co.uk>
2024-10-01 16:13:56 +0000

    Add button for cv download

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import danilleCv from '../assets/files/danilleCv.pdf'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <Row className='pt-2'>
+      <Col>
+      <div className='d-flex justify-content-start'>
+        <strong>Logo</strong></div>
+      </Col>
+      <Col>
+    <div className='d-flex justify-content-end'>
+      <a href={danilleCv} download="danilleCv" target='_blank' rel="noreferrer">
+      <button>Resume</button></a>
+    </div>
+    </Col>
+    </Row>
   )
 }
 
