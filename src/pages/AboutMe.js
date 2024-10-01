dc0aee7fcbac6a82e34f845461ec358f2add0711
Danille Hamilton <danilleham@hotmail.co.uk>
2024-10-01 16:15:03 +0000

    Remove name and button for resume

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -4,15 +4,11 @@ import { Row } from "react-bootstrap";
 const AboutMe = () => {
   return (
     <div>
-        <h1>
-        Danille Hamilton
-        </h1>
         <Row>
             <p>
                 A picture of me
             </p>
-        </Row>
-        <Row>
+        
     <p>
     Hi, I’m Danille, a software developer with a unique background in psychology and a passion for building intuitive, user-centered digital solutions. After earning my degree in Psychology, I developed a deep interest in how technology can enhance human experiences. This curiosity led me to pursue a diploma in Software Development, where I gained hands-on experience in modern programming languages and development frameworks.
     <br/>
@@ -22,7 +18,6 @@ const AboutMe = () => {
     <br/>
     I’m excited to combine my skills in problem-solving, critical thinking, and software development to tackle challenges in tech, and I’m always eager to learn and grow in this ever-evolving field.
     </p>
-    <button><p>Download resume</p></button>
         </Row>
     </div>
   )
