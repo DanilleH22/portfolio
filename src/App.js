36adb07f18497c76310d4fc0f6ed5fbd532b0cea
Danille Hamilton <danilleham@hotmail.co.uk>
2024-10-01 12:48:06 +0000

    add css styling

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css' 
 import ContactMe from './pages/ContactMe';
 import Experience from './pages/Experience';
+import styles from "../src/styles/Portfolio.module.css"
 
 function App() {
   return (
-    <Container fluid="md">
+    <Container fluid="md"
+    className='testing'
+    >
     <div className="App">
       <NavBar /> 
         <AboutMe />
