e8d86ee0bb20907d0acaef7b2c1b6c9034bc1421
Danille Hamilton <danilleham@hotmail.co.uk>
2024-10-01 22:35:10 +0000

    Add buttons for email me and style buttons

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -1,33 +1,54 @@
-import React from 'react'
+import React from 'react';
 import Row from 'react-bootstrap/Row';
-
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import { useEffect, useState } from 'react';
 
 export default function ContactMe() {
+  const [isLoading, setLoading] = useState(false);
 
-    let data = [
-        {
-            email: "hamiltonkdanille@hotmail.com",
-            linkedin: "www.linkedin.com/in/danille-hamilton-971b29206",
-            
-        }
-       
-    ]
+  useEffect(() => {
+    function simulateNetworkRequest() {
+      return new Promise((resolve) => setTimeout(resolve, 2000));
+    }
 
-    return (
-      <div>
-      <h1 className='heading'>Contact Me</h1>
-        {
-            data.map((item, index) => (
-                <Row eventKey={index.toString()} key={index}>
-                    <p>
-                        <strong> Email: </strong> {item.email}
-                    </p>
-<a href="www.linkedin.com/in/danille-hamilton-971b29206"> LinkedIn </a>
-                    
-                </Row>
-            ))
-        }
-    </div>
-    );
+    if (isLoading) {
+      simulateNetworkRequest().then(() => {
+        setLoading(false);
+      });
+    }
+  }, [isLoading]);
+
+  const handleClick = () => setLoading(true);
+
+  return (
+    <Row >
+      
+        <h1>Contact Me</h1>
+        <Col className='d-flex justify-content-end'>
+          <div>
+            <Button
+              href="https://www.linkedin.com/in/danille-hamilton-971b29206"
+              target="_blank"
+              variant="outline-secondary"
+            >
+              LinkedIn
+            </Button>
+          </div>
+        </Col>
+        <Col className='d-flex justify-content-start'>
+          <div>
+            <Button
+              variant="outline-secondary"
+              disabled={isLoading}
+              onClick={!isLoading ? handleClick : null}
+              href="mailto:hamiltonkdanille@hotmail.com"
+            >
+              {isLoading ? 'Loading…' : 'Email me'}
+            </Button>
+          </div>
+        </Col>
     
-} 
+    </Row>
+  );
+}
