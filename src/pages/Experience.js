1dc3fde889da6c9b16ae75bed9188b0c67287bb6
Danille Hamilton <danilleham@hotmail.co.uk>
2024-10-02 11:41:58 +0000

    Add css for experienece section

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -3,6 +3,8 @@ import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
+import styles from "../styles/Experience.module.css";
+import { useState } from 'react';
 
 function Experience() {
 
@@ -49,15 +51,18 @@ function Experience() {
       }
         ]
 
+        const [activeIndex, setActiveIndex] = useState(null);
+
   return (
     <Container className='p-5'>
       <Tab.Container  defaultActiveKey="0">
         <Row className="mt-3">
-          <Col sm={4}>
-            <Nav variant="outline-secondary" className="flex-column flex-row-reverse">
+          <Col sm={4} className='d-flex justify-content-end' style={{ fontSize: '12px' }}>
+            <Nav className={styles.Job}>
               {data.map((item, index) => (
-                <Nav.Item key={index}>
-                  <Nav.Link eventKey={index.toString()}>{item.job}</Nav.Link>
+                <Nav.Item key={index} className={`${styles.JobName} ${activeIndex === index ? styles.active : ''}`}
+                onClick={() => setActiveIndex(index)}>
+                  <Nav.Link eventKey={index.toString()} className='text-start' style={{ color: '#B8C6D9' }}>{item.job}</Nav.Link>
                 </Nav.Item>
               ))}
             </Nav>
@@ -66,9 +71,9 @@ function Experience() {
             <Tab.Content style={{ width: '80%' }}>
               {data.map((item, index) => (
                 <Tab.Pane eventKey={index.toString()} key={index} >
-                  <h5 className='d-flex justify-content-start ps-4'>{item.role}</h5>
+                  <h5 className='d-flex justify-content-start ps-4'><strong>{item.role}</strong></h5>
                   <p className='d-flex justify-content-start ps-4'>{item.date}</p>
-                  <ul style={{ listStyleType: 'circle', textAlign: 'left' }}> 
+                  <ul style={{ listStyleType: 'circle', textAlign: 'left', color: '#B8C6D9' }}> 
                     {item.description.map((point, i) => (
                       <li key={i}>{point}</li>
                     ))}
