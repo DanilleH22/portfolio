7342be1d7b7b225756ce434dc40a7f6604f92580
Danille Hamilton <danilleham@hotmail.co.uk>
2024-11-26 14:31:02 +0000

    Add Scroll animation and style nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,21 +2,32 @@ import React from 'react'
 import { Row } from "react-bootstrap";
 import { Col } from "react-bootstrap";
 import danilleCv from '../assets/files/danilleCv.pdf'
+import ScrollAnimation from '../components/ScrollAnimation';
+import styles from '../styles/Portfolio.module.css'
 
 const NavBar = () => {
   return (
-    <Row className='pt-2'>
+    <ScrollAnimation>
+    <Row className='pt-2' style={{ fontSize: '25px' }}>
+      
       <Col>
-      <div className='d-flex justify-content-start'>
-        <strong>Logo</strong></div>
+      <div className={styles.Name} >
+        <strong>Danille Hamilton</strong></div>
       </Col>
       <Col>
-    <div className='d-flex justify-content-end'>
+      <div>
+        <strong>Junior Software Developer</strong>
+      </div>
+      </Col>
+      <Col style={{ fontSize: '20px' }}>
+    <div className={styles.Resume}>
       <a href={danilleCv} download="danilleCv" target='_blank' rel="noreferrer">
-      <button>Resume</button></a>
+      <button><strong>Resume</strong></button></a>
     </div>
     </Col>
+    
     </Row>
+    </ScrollAnimation>
   )
 }
 
