1dcb2cde9b75e28bc8783b108194dbfb3767f630
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2025-06-13 23:12:44 +0100

    Add container class for experience

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -57,7 +57,7 @@ function Experience() {
         const [activeIndex, setActiveIndex] = useState(null);
 
         return (
-          <Container fluid >
+          <Container fluid className={styles.Container}>
             <ScrollAnimation animationVariants={{
     hidden: { opacity: 0, x: -100 },
     visible: { opacity: 1, x: 0, transition: { duration: 1 } }
