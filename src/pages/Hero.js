58377e81ed9bb3df4176bae3969540025df97025
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2026-01-22 18:57:09 +0000

    create hero componenet and page

---
+++ src/pages/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TextFrontCarousel from '../components/TextFrontCarousel';
+
+const Hero = () => {
+  return (
+    <main style={{ overflow: "visible" }}>
+        
+
+    <TextFrontCarousel />
+    </main>
+  )
+}
+
+export default Hero
