de6fe8c998f5c383137b086ef5ebde3373fe2122
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2026-01-22 18:57:42 +0000

    update to inc new about (hero)

--- src/App.js
+++ src/App.js
@@ -9,22 +9,28 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import ContactMe from './pages/ContactMe';
 import Experience from './pages/Experience';
 import styles from "../src/styles/Portfolio.module.css"
+import About2 from './pages/AboutMe2';
+import Hero from './pages/Hero';
 
 function App() {
   return (
+    <>
+    <NavBar /> 
+      <Hero />
     <Container fluid="md"
-    className='testing'
+    className='App'
     style={{ width: '100%', overflow: 'hidden'}}
     >
-    <div className="App">
-      <NavBar /> 
-        <AboutMe />
+      
+      <About2 />
+        {/* <AboutMe /> */}
         <Technologies />
         <Projects />
         <Experience />
         <ContactMe />
-    </div>
+
     </Container>
+    </>
   );
 }
 
